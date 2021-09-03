a994469ea1575cf0fa73e19e163a101632774494
soikat15 <soikathasan15@gmailcom>
2021-09-04 05:31:18 +0900

    same file to save results

--- src/utils/save_utils.py
+++ src/utils/save_utils.py
@@ -1,25 +1,32 @@
 import os
 import csv
+from datetime import datetime
 
 
 def save_results(config, metrics, name=None):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+
     if not os.path.exists(config['save_results_path']):
         os.mkdir(config['save_results_path'])
-    if name is None:
-        save_file_name = config['save_file_name'] + \
-                         '_' + config['dataset_name'] + \
-                         '_' + config['model_name'] + '.csv'
-    else:
-        save_file_name = name
+
+    save_file_name = config['save_file_name'] + config['dataset_name'] + config['model_name'] + '.csv'
+
     if not os.path.exists(os.path.join(config['save_results_path'], save_file_name)):
         os.mknod(os.path.join(config['save_results_path'], save_file_name))
     result_file_path = os.path.join(config['save_results_path'], save_file_name)
 
-    with open(result_file_path, 'w') as csv_file:
+    with open(result_file_path, 'a') as csv_file:
         writer = csv.writer(csv_file)
+        writer.writerow(['Dataset Name:', config['dataset_name']])
+        writer.writerow(['Model Name', config['model_name']])
+        writer.writerow(['Sub Folders', name])
+        writer.writerow(['Record Time:', current_time])
+        writer.writerow([])
         for key, value in metrics.items():
             writer.writerow([key, value.data.item()])
-
+        writer.writerow([])
+        writer.writerow([])
     print("\n"
           "Saved metric in {}."
           "\n".format(config['save_results_path']))
